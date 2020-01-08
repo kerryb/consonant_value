3e9395d903d2980e31bc8b43c66d8b1bdbe4ced0
Kerry Buckley <kerryjbuckley@gmail.com>
2020-01-08 14:31:14 +0000

    New mix project

---
+++ lib/consonant_value.ex
@@ -0,0 +1,18 @@
+defmodule ConsonantValue do
+  @moduledoc """
+  Documentation for ConsonantValue.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ConsonantValue.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/consonant_value_test.exs
@@ -0,0 +1,8 @@
+defmodule ConsonantValueTest do
+  use ExUnit.Case
+  doctest ConsonantValue
+
+  test "greets the world" do
+    assert ConsonantValue.hello() == :world
+  end
+end
